fedfa21620e84815983451b08086d200a3eaf861
lioliadoc <liolia.doc@gmail.com>
2025-01-12 00:21:07 -0600

    Rotate list complete

--- rotate_list.py
+++ rotate_list.py
@@ -1,4 +1,12 @@
 # Add your clarifying questions here
 
 def rotate_list(list, shift_by):
-    pass
+    for i in range(shift_by):
+        popped_element = list.pop()
+        list.insert(0, popped_element)
+    return list
+
+assert rotate_list([1, 2, 3, 4, 5], 3) == [3, 4, 5, 1, 2]
+print("tests passed")
+
+    
