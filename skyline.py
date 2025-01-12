5ca592c43d8355c2e8da7f2eb8e28226af2b8064
lioliadoc <liolia.doc@gmail.com>
2025-01-12 00:27:15 -0600

    skyline problem solved

--- skyline.py
+++ skyline.py
@@ -1,4 +1,11 @@
 # Add your clarifying questions here
 
 def skyline(building_list):
-    pass
+    build_set = set()
+    for i in range(len(building_list)):
+        if building_list[i] > 0:
+            build_set.add(building_list[i])
+    return sorted(list(build_set))
+
+assert skyline([-1, 1, 3, 7, 7, 3]) == [1,3,7]
+print("tests passed")
